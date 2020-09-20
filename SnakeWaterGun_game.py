670312dcfea28dd32dfcc495c7b24eb8ec5b19d6
Prakhar Srivastava <prakharsrivastava.vms97@gmail.com>
2020-09-20 14:47:37 +0530

    Correcting point codes

--- SnakeWaterGun_game.py
+++ SnakeWaterGun_game.py
@@ -4,8 +4,9 @@ options = {"s": "Snake", "w": "Water", "g": "Gun"}
 
 def games():
     no_of_games = 1
-    points1 = 2
-    points2 = 2
+    points1 = 0
+    points2 = 0
+    print("\t \t \t \t \t \t \t \t \t \tSnake Water Gun\n \n")
     while no_of_games <= 10:
 
         print("Select your player: ")
@@ -13,36 +14,33 @@ def games():
             print("Press", key, "for", value, "\n", end="")
 
         options_name = input()
-        # if options_name is not "s" or "w" or "g":
-        #     print("Wrong Input!")
-        #     options_name = input("Select your player: ")
 
         print("User Player : ", options[options_name], "\n", end="")
         lis = list(options.values())
         choice = random.choice(lis)
         print("Computer Player : ", choice, "\n", end="")
         if choice == options[options_name]:
-            print("Try again!")
+            print("Try again! \n \n")
         else:
             if choice == "Snake" and options[options_name] == "Water":
-                print(choice, "Computer Wins!", points1, "points earned.", "\n", end="")
-                print()
                 points1 += 1
+                print(choice, "Computer Wins!", points1, "points earned.", "\n", end="")
+                print("\n")
+
             elif choice == "Water" and options[options_name] == "Gun":
-                print(choice, "Computer Wins!", points1, "points earned.", "\n", end="")
-                print()
                 points1 += 1
+                print(choice, "Computer Wins!", points1, "points earned.", "\n", end="")
+                print("\n")
+
             elif choice == "Gun" and options[options_name] == "Snake":
-                print(choice, "Computer Wins!", points1, "points earned.", "\n", end="")
-                print()
                 points1 += 1
+                print(choice, "Computer Wins!", points1, "points earned.", "\n", end="")
+                print("\n")
 
             else:
+                points2 += 1
                 print(options[options_name], "Player Wins!", points2, "points earned.", "\n", end="")
                 print()
-                points2 += 1
-
-                # points1 += 1
 
         print(no_of_games, " Numbers of Games Played.")
         no_of_games += 1
